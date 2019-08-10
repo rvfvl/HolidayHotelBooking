1ffec7687c4816b107f4df94e967ff596bf684af
Rafal <rafal.malewicz1@gmail.com>
2019-08-10 20:27:18 +0100

    Initial styling for single hotel page.

--- src/pages/SingleHotel.js
+++ src/pages/SingleHotel.js
@@ -1,9 +1,40 @@
 import React, { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { fetchSingleHotel } from "actions"
+import styled from "styled-components"
+import Button from "components/Button"
+
+const MainImageWrapper = styled.div`
+  position: relative;
+
+  img {
+    object-fit: cover;
+    width: 100vw;
+    height: 300px;
+  }
+
+  button {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    border: 1px solid #fff;
+    transform: translate(-50%, -50%);
+  }
+`
+
+const HotelDetailsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 2rem;
+
+  div {
+    margin: 0 5rem;
+  }
+`
 
 const SingleHotel = props => {
   const {
+    history: { goBack },
     match: {
       params: { id }
     }
@@ -11,14 +42,50 @@ const SingleHotel = props => {
 
   const dispatch = useDispatch()
 
-  const hotelDetails = useSelector(state => state.singleHotel)
-
   useEffect(() => {
     dispatch(fetchSingleHotel(id))
   }, [])
 
+  const hotelDetails = useSelector(state => state.singleHotel)
+
+  const {
+    fields: {
+      city,
+      hotelName,
+      pricePerNight,
+      stars,
+      street,
+      noOfBathrooms,
+      noOfRooms,
+      image: {
+        fields: {
+          file: { url }
+        }
+      }
+    }
+  } = hotelDetails
+
   return (
-    <div>Single hotel page {console.log(hotelDetails.fields.hotelName)}</div>
+    <>
+      <MainImageWrapper>
+        <img src={url} alt={hotelName} />
+        <Button onClick={goBack}>Go Back</Button>
+      </MainImageWrapper>
+
+      <HotelDetailsWrapper>
+        <div>
+          <h1>{hotelName}</h1>
+          <p>City: {city}</p>
+          <p>Street: {street}</p>
+        </div>
+        <div>
+          <p>Price: ${pricePerNight} per night</p>
+          <p>Rating: {stars} stars</p>
+          <p>Number of rooms: {noOfRooms}</p>
+          <p>Number of bathrooms: {noOfBathrooms}</p>
+        </div>
+      </HotelDetailsWrapper>
+    </>
   )
 }
 
